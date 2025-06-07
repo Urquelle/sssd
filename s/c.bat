3a97d53062f86065a5d0fc5121cbf10cab27ad29
Serge Ratke <serge.ratke@gmail.com>
2025-06-07 07:56:56 +0200

    clang erstellung angepasst.

--- s/c.bat
+++ s/c.bat
@@ -8,7 +8,7 @@ set E=%PP%\e
 
 set CC=clang.exe
 
-set CCFLAGS=-O0 -fexec-charset=utf-8 -D_CRT_SECURE_NO_WARNINGS -std=c17 -fno-exceptions -fno-rtti -g -Werror -Wall -Wextra -Wno-microsoft-enum-forward-reference -Wno-unused-parameter -Wno-unused-variable -Wno-missing-field-initializers -Wno-constant-logical-operand -Wno-switch -Wno-unused-but-set-variable -Wno-logical-op-parentheses -FC -I%PP%/q -I%PP%/t
+set CCFLAGS=-O0 -fexec-charset=utf-8 -D_CRT_SECURE_NO_WARNINGS -std=c17 -fno-exceptions -fno-rtti -g -Werror -Wall -Wextra -Wno-microsoft-enum-forward-reference -Wno-unused-parameter -Wno-unused-variable -Wno-missing-field-initializers -Wno-constant-logical-operand -Wno-switch -Wno-sign-compare -Wno-pointer-to-int-cast -Wno-unused-but-set-variable -Wno-logical-op-parentheses -FC -I%PP%/q -I%PP%/t
 set LDFLAGS=
 
 rmdir /Q /S %E%
